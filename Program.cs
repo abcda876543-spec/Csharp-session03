0ccfe1d6e5988664193a748a47a8d8cb90e704f2
menna <abcda876543@gmail.com>
2026-09-08 03:21:11 +0300

    Question 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace C__Session03
 {
+    class Book {
+        string Title;
+         int Pages;
+    }
     internal class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1 
+            Book myBook = new Book();
+            object obj = myBook;
+            Console.WriteLine(obj);
+            #endregion
         }
     }
 }
