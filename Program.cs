d0a3d77589466b242c99859fda41123199a0f6db
menna <abcda876543@gmail.com>
2026-09-08 03:58:19 +0300

    Question 4

--- Program.cs
+++ Program.cs
@@ -25,8 +25,24 @@
             #region Question 3 
             //it is a compile time error
             int pages = 464;
-           
+
             #endregion
+
+            #region Question 4
+            try
+            {
+                int number = 10;
+                int number1 = 0;
+                int result = number / number1;
+            }
+            catch (DivideByZeroException) {
+                Console.WriteLine("cannot divide by zero");
+                    }
+            finally {
+                Console.WriteLine("Done");
+                    }
+            #endregion
+
         }
     }
 }
